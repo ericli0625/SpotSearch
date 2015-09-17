ee121a189ebeaa51157bd0357d4ccb6e7b151b1e
eric.hc_li <eric.hc_li@htc.com>
2015-09-17 20:18:34 +0800

    Edit admin

--- spots/admin.py
+++ spots/admin.py
@@ -8,10 +8,13 @@ admin.site.register(Book)
 
 class TotalspotsAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 	list_display = ('name','category','cities','city','address','content')
+	search_fields = ('name', 'cities' , 'city')
+	list_filter = ('cities',)
 
 admin.site.register(Totalspots,TotalspotsAdmin)
 
 class CitiesAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 	list_display = ('cities','city')
+	search_fields = ('cities', 'city')
 
 admin.site.register(Cities,CitiesAdmin)
