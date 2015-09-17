62aee400008631cf074b4ec2844b445557d97480
eric.hc_li <eric.hc_li@htc.com>
2015-09-17 11:57:50 +0800

    Fixed Search iuuse and Create Google calendar link

--- Eric/settings.py
+++ Eric/settings.py
@@ -57,18 +57,18 @@ WSGI_APPLICATION = 'Eric.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-#import dj_database_url
-#DATABASES = {'default': dj_database_url.config(default='postgres://qiivcofeurbrrq:ZrHRbAnPUaJTKo3lhyGCEKMMlI@ec2-54-83-204-104.compute-1.amazonaws.com:5432/d5sloc17vjd4sd')}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://qiivcofeurbrrq:ZrHRbAnPUaJTKo3lhyGCEKMMlI@ec2-54-83-204-104.compute-1.amazonaws.com:5432/d5sloc17vjd4sd')}
 
 
-DATABASES = {
-    'default': {
+#DATABASES = {
+#    'default': {
         #'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'travel_db',
-    }
-}
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'NAME': 'travel_db',
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
